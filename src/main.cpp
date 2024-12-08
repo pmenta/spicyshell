cbf82ff9cb35631e83c925c40f81206b6f7ce41c
João Martins <joao.martinsm@pm.me>
2024-12-08 03:38:37 -0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <sstream>
+#include <vector>
 
-[[noreturn]] int main() {
+std::vector<std::string> split(const std::string &s, char delim) {
+  std::vector<std::string> elems;
+  std::string element;
+  std::istringstream elemsStream(s);
+
+  while (std::getline(elemsStream, element, delim)) {
+    elems.push_back(element);
+  }
+
+  return elems;
+}
+
+int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
@@ -10,6 +24,14 @@
 
     std::string input;
     std::getline(std::cin, input);
+
+    std::vector<std::string> tokens = split(input, ' ');
+
+    if (!tokens.empty()) {
+      if (tokens[0] == "exit") {
+        return std::stoi(tokens[1]);
+      }
+    }
 
     std::cout << input << ": command not found\n";
   }
