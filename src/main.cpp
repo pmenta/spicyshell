727f3ef65a7e42c174fa463dae55737ef9670fd9
João Martins <joao.martinsm@pm.me>
2024-12-08 22:57:30 -0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,19 @@ void echo(const std::string& text) {
   std::cout << text << std::endl;
 }
 
+enum class Builtin { echo, exit, type, unknown };
+Builtin parseCommand(const std::string& command) {
+  if (command == "echo") {
+    return Builtin::echo;
+  } else if (command == "exit") {
+    return Builtin::exit;
+  } else if (command == "type") {
+    return Builtin::type;
+  } else {
+    return Builtin::unknown;
+  }
+}
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
@@ -31,15 +44,24 @@ int main() {
 
     std::vector<std::string> tokens = split(input, ' ');
     if (!tokens.empty()) {
-      if (tokens[0] == "echo") {
+      Builtin command = parseCommand(tokens[0]);
+      if (command == Builtin::echo) {
         std::string params;
         for (int i = 1; i < tokens.size(); i++) {
           params += i == 1 ? tokens[i] : " " + tokens[i];
         }
-
         echo(params);
-      } else if  (tokens[0] == "exit") {
+      } else if (command == Builtin::exit) {
         return std::stoi(tokens[1]);
+      } else if (command == Builtin::type) {
+        Builtin arg = parseCommand(tokens[1]);
+        if (arg == Builtin::unknown) {
+            std::cout << tokens[1] + ": not found\n";
+        } else {
+          std::cout << tokens[1] + " is a shell builtin\n";
+        }
+
+
       } else {
         std::cout << input << ": command not found\n";
       }
