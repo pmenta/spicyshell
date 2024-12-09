a02dd9a8604381d5fd4e628bbef59af0510b9bfa
João Martins <joao.martinsm@pm.me>
2024-12-09 02:28:04 -0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
+#include <complex>
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <filesystem>
+namespace fs = std::filesystem;
 
-std::vector<std::string> split(const std::string &s, char delim) {
+using Args = std::vector<std::string>;
+
+Args split(const std::string &s, const char delim) {
   std::vector<std::string> elems;
   std::string element;
   std::istringstream elemsStream(s);
@@ -31,6 +36,38 @@ Builtin parseCommand(const std::string& command) {
   }
 }
 
+void type(const Args& tokens) {
+  const Builtin arg = parseCommand(tokens[1]);
+  std::string pathToArg;
+
+  if (const char* pathEnvVar = std::getenv("PATH"); pathEnvVar) {
+    const std::string path = pathEnvVar;
+    std::vector<std::string> paths = split(path, ':');
+    for (const std::string& path : paths) {
+      if (!pathToArg.empty()) break;
+      for (const auto& entry : fs::directory_iterator(path)) {
+        if (!pathToArg.empty()) break;
+        auto vecDir = split(entry.path().string(), '/');
+        const auto& lastItem = vecDir[vecDir.size() - 1];
+        if (lastItem == tokens[1]) {
+          pathToArg = entry.path().string();
+          break;
+        }
+      }
+    }
+  }
+
+  if (arg != Builtin::unknown) {
+    std::cout << tokens[1] + " is a shell builtin\n";
+  } else {
+    if (pathToArg.empty()) {
+      std::cout << tokens[1] + ": not found\n";
+    } else {
+      std::cout << tokens[1] + " is " + pathToArg + "\n";
+    }
+  }
+}
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
@@ -42,10 +79,8 @@ int main() {
     std::string input;
     std::getline(std::cin, input);
 
-    std::vector<std::string> tokens = split(input, ' ');
-    if (!tokens.empty()) {
-      Builtin command = parseCommand(tokens[0]);
-      if (command == Builtin::echo) {
+    if (std::vector<std::string> tokens = split(input, ' '); !tokens.empty()) {
+      if (const Builtin command = parseCommand(tokens[0]); command == Builtin::echo) {
         std::string params;
         for (int i = 1; i < tokens.size(); i++) {
           params += i == 1 ? tokens[i] : " " + tokens[i];
@@ -54,14 +89,7 @@ int main() {
       } else if (command == Builtin::exit) {
         return std::stoi(tokens[1]);
       } else if (command == Builtin::type) {
-        Builtin arg = parseCommand(tokens[1]);
-        if (arg == Builtin::unknown) {
-            std::cout << tokens[1] + ": not found\n";
-        } else {
-          std::cout << tokens[1] + " is a shell builtin\n";
-        }
-
-
+        type(tokens);
       } else {
         std::cout << input << ": command not found\n";
       }
